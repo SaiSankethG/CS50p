41d8582b654d210fe00749153dde2294376d830a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-19 14:31:56 +0000

    Wed, Oct 19, 2022, 2:31 PM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,26 +1,18 @@
-# def main():
-#     dollars = dollars_to_float(input("How much was the meal? "))
-#     percent = percent_to_float(input("What percentage would you like to tip? "))
-#     tip = dollars * percent
-#     print(f"Leave ${tip:.2f}")
-
-
-# def dollars_to_float(d):
-#     d1=d.replace("$" , "")
-#     return d1
-
-# def percent_to_float(p):
-#     return int(p.replace("%" , ""))/100
-
-# main()
 def main():
-    dollars=float(input("how much was the meal?"))
-    percent=p_to_float(input("What percentage would you like to tip?"))
-    tip=dollars*percent
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-def p_to_float(p):
-    return float(p.replace("%" , ""))/100
+
+def dollars_to_float(d):
+    d1=d.replace("$" , "")
+    return float(d1)
+
+
+def percent_to_float(p):
+    p1=p.replace("%" , "")
+    p2=float(p1)/100
+    return p2
 
 main()
-
