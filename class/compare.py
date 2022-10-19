61d48b0bf9654aa134c413a3feb733ce9380173a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-19 14:56:42 +0000

    Wed, Oct 19, 2022, 2:56 PM +00:00

--- class/compare.py
+++ class/compare.py
@@ -1,1 +1,11 @@
 x=int(input("whats is x?"))
+y=int(input("whats is y?"))
+
+if x<y:
+    print("x is less than y")
+
+if x>y:
+    print("x is greater than y")
+
+if x==y:
+    print("x is equal than y")
