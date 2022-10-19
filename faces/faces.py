f46b002c907dd1d902a4b466bfb960ec3cee0944
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-19 02:15:48 +0000

    Wed, Oct 19, 2022, 2:15 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,5 +1,18 @@
-face=input()
-new_face=face.split(" ")
-emoji={
+# face=input()
+# new_face=face.split(" ")
+# emoji={
+
+# }
+def main()
+  msg=input()
+  result=convert(msg)
+  print(result)
+
+
+def convert(msg):
     
-}
+
+
+
+
+main()
