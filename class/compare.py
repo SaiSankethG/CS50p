898dfdc58ebc9d72091382b958a0f819a3034dad
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-20 17:32:38 +0000

    Thu, Oct 20, 2022, 5:32 PM +00:00

--- class/compare.py
+++ class/compare.py
@@ -1,11 +1,19 @@
-x=int(input("whats is x?"))
-y=int(input("whats is y?"))
+# x=int(input("whats is x?"))
+# y=int(input("whats is y?"))
 
-if x<y:
-    print("x is less than y")
+# if (x<y):
+#     print("x is less than y")
 
-elif x>y:
-    print("x is greater than y")
+# elif x>y:
+#     print("x is greater than y")
 
-else :
-    print("x is equal than y")
+# else :
+#     print("x is equal than y")
+
+x=input("whats is x?")
+y=input("whats is y?")
+
+if x<y or x>y:
+    print("x is not equal to y")
+else
+    print("x is equal to y")
