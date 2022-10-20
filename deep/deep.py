b38f6693104aa2c0c1f3b8f8a690c761839e25e9
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-20 18:48:06 +0000

    Thu, Oct 20, 2022, 6:48 PM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,12 @@
+uni=input("What is the answer to the great question of life, the Universe , and Everything?")
+
+# if uni=:
+#     print("Yes")
+# else :
+#     print("No")
+
+match uni:
+    case "42" | "forty-two" |"forty two":
+        print("Yes")
+    case _:
+        print("No")
