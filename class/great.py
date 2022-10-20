c7037cac08d0044d898fcb1c3e0b854ab101b095
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-20 18:04:19 +0000

    Thu, Oct 20, 2022, 6:04 PM +00:00

--- class/great.py
+++ class/great.py
@@ -1,1 +1,12 @@
 score=int(input("Score:"))
+
+if x>=90 and x<=100:
+    print("A")
+elif x>=80 and x<90:
+    print("B")
+elif x>=70 and x<80:
+    print("C")
+elif x>=60 and x<70:
+    print("D")
+else:
+    print("F")
