9edaab0d15e275c69f4b3823cd8e492361a64efc
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-21 08:57:16 +0000

    Fri, Oct 21, 2022, 8:57 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,10 @@
-uni=input("What is the Answer to the Great question of life, the Universe and Everything?")
+uni=input("What is the Answer to the Great question of life, the Universe and Everything?").lower()
 
-if uni=="42" or uni=="forty-two" or uni=="forty two":
-   print("Yes")
+if uni=="42":
+    print("Yes")
+elif uni=="forty-two":
+    print("Yes")
+elif uni=="forty two":
+    print("Yes")
 else:
-   print("No")
+    print("No")
