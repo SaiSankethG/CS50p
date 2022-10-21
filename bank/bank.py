2f36535aeb8f28b5ce38aec32c7ca63402d3d9ec
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-21 09:21:32 +0000

    Fri, Oct 21, 2022, 9:21 AM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,9 @@
 amount=input()
 dollar=amount.lower()
+
 if dollar.split()=="hello":
    print("$0")
-elif dollar[0]=="h" and dollar.split!="hello":
-   print("$20")
+# elif dollar[0]=="h" and dollar.split()!="hello":
+#    print("$20")
 else:
    print("$100")
