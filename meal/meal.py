56044b4691ad597b74c29298222c22e69e7cb114
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-22 03:04:25 +0000

    Sat, Oct 22, 2022, 3:04 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,14 @@
 def main():
     time=input("What time is it?")
     t=time.replace(":" , ".")
-    t1=float(t)
+    t=float(t)
 
-if 7.00 >= t<= 8.00:
+if 7.00 >t and t<= 8.00:
     print("Breakfast time")
 elif 12.00 >=t <=13.00:
     print("Lunch time")
-elif 18.00>=t <=19.00:
+elif 18.00>=t<=19.00:
     print("dinner time")
-else :
-    return 0
+
 
 main()
