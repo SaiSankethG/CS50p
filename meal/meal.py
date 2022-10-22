6ed8cc875473fc11709c5b8906edb2e223ce942d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-22 03:38:07 +0000

    Sat, Oct 22, 2022, 3:38 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,17 @@
  def main():
     time=input("Whats the time?")
-    convert(time)
+    hours , minutes=convert(time)
+    if hours>=7 and hours+minutes<=8:
+       print("Breakfast time")
+    elif hours>=12 and hours+minutes<=13:
+       print("Breakfast time")
+    elif hours>=7 and hours+minutes<=8:
+       print("Breakfast time")
+
 
 
 def convert(time):
-    
-
+    return hours , minutes =time.split(":")
 
 if __name__ == "__main__":
-    main()d
+    main()
