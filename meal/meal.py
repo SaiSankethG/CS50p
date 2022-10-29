eeefc977739ca777779bb375a65c4b580e5d278b
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-29 15:53:04 +0000

    Sat, Oct 29, 2022, 3:53 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,3 @@
 def main():
-  time=input("What time is it? ")
-  hours , minutes =time.split(":")
-  minute=float(minutes)/60
-  hour=float(hours)
-  if hour>=7 and hour+minute<=8:
-    print("breakfast time")
-  elif hour>=12 and hour+minute<=13:
-    print("lunch time")
-  elif hour>=18 and hour+minute<=19:
-    print("dinner time")
-
-
-main()
+  answer=input("What time is it?")
+  
