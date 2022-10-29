a822c1c93c427689e49f1fee4e45ecedf34c07e4
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-29 16:00:38 +0000

    Sat, Oct 29, 2022, 4:00 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,6 +1,12 @@
 def main():
   answer=input("What time is it?")
   time=convert(answer)
+  if time>=7 and time<=8:
+    print("breakfast time")
+  elif time>=12 and time<=13:
+    print("lunch time")
+  else time>=18 and time<=19:
+    print("dinner time")
 
 
 def convert(time):
