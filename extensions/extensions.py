5ebe88ccdd4aa379dd7bcc79469c774eb4bd8278
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-10-29 16:25:06 +0000

    Sat, Oct 29, 2022, 4:25 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,20 @@
 name=input("File name:")
+new_name=name.lower()
+if '.gif' in new_name:
+    print("image/gif")
+elif '.png' in new_name:
+    print("image/png")
+elif '.jpg' in new_name:
+    print("image/jpg")
+elif '.jpeg' in new_name:
+    print("image/jpeg")
 
+if '.pdf' in new_name:
+    print("application/type")
+elif '.zip' in new_name:
+    print("application/type")
+
+if '.txt' in new_name:
+    print("text/plain")
+else:
+    print("application/octet-stream")
