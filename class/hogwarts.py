7b89bf3b45be7bed21946c195a4324d0e603d707
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-02 05:26:55 +0000

    Wed, Nov 2, 2022, 5:26 AM +00:00

--- class/hogwarts.py
+++ class/hogwarts.py
@@ -1,4 +1,5 @@
 students=[{"name":"Heromine" ,"house":"gryffindor" ,"patronus":"otter"},
           {"name":"Harry" , "house":"gryffindor" , "patronus":"stag"},
           {"name":"Ron" , "house":"gryffindor" , "patronus":"jack russell terrir"} ,
-          {"name":"Draco" , "house":"slytherin" , "patronus":"none"}]
+          {"name":"Draco" , "house":"slytherin" , "patronus":None}
+          ]
