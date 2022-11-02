95df8d664bac3f6e6e68710ae9a317b38d9d9f5a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-02 03:20:17 +0000

    Wed, Nov 2, 2022, 3:20 AM +00:00

--- class/hogwarts.py
+++ class/hogwarts.py
@@ -1,7 +1,7 @@
 students=["heromine" , "harry" , "ron"]
 
-# for i in range(3):
-#     print(students[i])
+for i in range(len(students)):
+    print(students[i])
 
-for student in students:
-    print(student)
+# for student in students:
+#     print(student)
