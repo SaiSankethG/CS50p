b00c4ffb36a8acec439d27753d5b729a9479b19d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-06 18:06:36 +0000

    Sun, Nov 6, 2022, 6:06 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,12 +1,17 @@
-def main():
-    camel_case=input("camelCase:")
-    print("snake_case:")
-    for i in range(len(camel_case)):
-       if (camel_case[i]>="A" and camel_case[i]<="Z"):
-        snake_case=camel_case[i-1].replace("" , "_")
-        snake_case=camel_case[i].lower()
-        print(snake_case ,end="")
-       else:
-         print(snake_case , end="")
+# def main():
+#     camel_case=input("camelCase:")
+#     print("snake_case:")
+#     for i in range(len(camel_case)):
+#        if (camel_case[i]>="A" and camel_case[i]<="Z"):
+#         snake_case=camel_case[i-1].replace("" , "_")
+#         snake_case=camel_case[i].lower()
+#         print(snake_case ,end="")
+#        else:
+#          print(snake_case , end="")
 
-main()
+# main()
+
+camelCase=input("camelCase:")
+print("snake_case:" ,end="")
+for i in camelCase:
+  if
