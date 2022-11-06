ed696e1f664501b50915939d004ebd74d72bd12f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-06 19:13:55 +0000

    Sun, Nov 6, 2022, 7:13 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,9 +4,13 @@ def main():
     print("Amount Due: " , amount_due)
     while(amount_due>0 or amount_due<50):
         insert_coin=int(input("Insert Coin: "))
-        total_insert=insert
+        total_insert=insert_coin
         amount_due-=insert_coin
-        print("Insert Coin:" , insert_coin)
+        # print("Insert Coin:" , insert_coin)
+        if total_insert>50:
+            change_owed=total_insert-50
+            print("Changed Owed: " , change_owed)
+        else:
+            print("Changed Owed: 0")
 
-
-
+main()
