1598767651d8b52e4dd4bcb4a737c23050e7fa63
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-08 13:35:44 +0000

    Tue, Nov 8, 2022, 1:35 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,9 +8,14 @@ def main():
 
 def is_valid(s):
     if 6>=len(s)>=2 and s[0:2].isalpha() ands.isalnum():
+        for char in s:
+            if char.isdigit():
+                index=s.index(char)
+                if s[index:].isdigit() and int(char)!=0:
+                    return True
+                else:
+                    return False
         return True
-    else:
-        return False
-    
+
 
 main()
