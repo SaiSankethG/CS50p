5d8fa1dffdd846af9136f77ccb4de99375c9e4c2
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-09 14:44:13 +0000

    Wed, Nov 9, 2022, 2:44 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,15 +1,3 @@
-def main():
-    while True:
-        try:
-            x, y =int(input("Fraction:")).split()
-            print(f"x is {x} , y is {y}")
-        except (ValueError , ZeroDivisionError):
-            continue
-        else:
-            break
-
-
-
-
-main()
+while True:
+    
 
