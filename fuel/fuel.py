ba8a1f934edb5b1950cd07a29bda41c412564419
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-09 14:53:00 +0000

    Wed, Nov 9, 2022, 2:53 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,18 @@
 while True:
     try:
         x, y =int(input("Fraction:")).split("/")
+        f=x/y
+        if f<=1:
+            break
     except(ValueError , ZeroDivisionError):
         continue
+
+per=f*100
+if 0<per<1 :
+    print("E")
+elif 99<per<=100:
+    print("F")
+else:
+    print(f"{per}%")
 
 
