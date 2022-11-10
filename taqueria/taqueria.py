70ca987ecf8f65fd81a912f391f4d26adab1144b
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-10 02:01:46 +0000

    Thu, Nov 10, 2022, 2:01 AM +00:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,28 +1,24 @@
-menu={
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-total_amount=0
+def main():
+    menu={
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+    total_amount=0
 
-while True:
-    try:
-        order=input("Item: ").title()
-        print(order)
-        if order in menu:
-            print(order)
-            total_amount+=menu[order]
-            print("Total: $" , end="")
-            print(total_amount)
-    except EOFError:
-        print()
-        break
+    while True:
+        try:
+            order= input ("Item: ").title()
+            item=amount(order)
+        except EOFError:
+            print()
+            break
 
 
 
