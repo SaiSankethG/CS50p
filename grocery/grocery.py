692d29655590b8e4d4f65e9a697369169ef767c5
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-11 04:52:51 +0000

    Fri, Nov 11, 2022, 4:52 AM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,24 +1,4 @@
-def main():
-    dict={}
-    while True:
-        try:
-            name=input()
-            value=1
-            dict[value]=name
-        except EOFError:
-            list()
-            print()
-            break
-def list():
-    for i in dict:
-        print(i[value] , i[name] , sep=" ")
-main()
-
-
-
-
-
-
-# calling for printing
-# making it alphabetical order
-# updating and printing the list
+grocery={}
+while True:
+    item=input().upper()
+    
