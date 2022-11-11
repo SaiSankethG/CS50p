04a1c3db4209f6936e3579e8e11a3921de8d032d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-11 15:10:52 +0000

    Fri, Nov 11, 2022, 3:10 PM +00:00

--- class/name.py
+++ class/name.py
@@ -1,8 +1,8 @@
 from sys import argv
 
 if len(argv)<2:
-    print("Too few arguments")
+    exit("Too few arguments")
 elif len(argv)>2:
-    print("Too many arguments")
-else:
-    print("Hello , my name is" , argv[1])
+    exit("Too many arguments")
+
+print("Hello , my name is" , argv[1])
