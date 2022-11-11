46da88f60515a5279dc495f1cfc16d7387820df5
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-11 05:30:30 +0000

    Fri, Nov 11, 2022, 5:30 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,5 +1,24 @@
-#list of month
-
-#infinite loop
-    #asking for user input
-        #
+month=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+while True:
+    try:
+        date=input("Date:")
+        m ,d , y=date.split(" ")
+        if m in month:
+            if d>31:
+                continue
+            else:
+                print(f"{y}-{m}-{d}")
+    except
