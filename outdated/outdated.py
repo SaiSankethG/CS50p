8c23eb475a0c9071f7b7e96af9534ed9037a2702
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-11 08:44:30 +0000

    Fri, Nov 11, 2022, 8:44 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,21 +15,20 @@ month=[
 while True:
     date=input("Date:").title()
     try:
-        m,d,y=date.split("/")
+        m, d,y=date.split("/")
         if (1>=int(m)<=12 and 1>=int(d)<=31):
-            print(f"{y}-{int(m):02}-{int(d):02}")
             break
     except:
         try:
-            m1 , d1,  y1=date.split(" ")
+            m1 , d1,  y=date.split(" ")
             for i in range(len(month)):
                 if m1==month:
-                    m2=i
-            d2=d1.replace("," , "")
-            if (1>=int(m2)<=12 and 1>=int(d2)<=31):
-                print(f"{y1}-{int(m2):02}-{int(d2):02}")
+                    m=i
+            d=d1.replace("," , "")
+            if (1>=int(m)<=12 and 1>=int(d)<=31):
                 break
         except EOFError:
             print()
             pass
 
+print(f"{y}-{int(m):02}-{int(d):02}")
