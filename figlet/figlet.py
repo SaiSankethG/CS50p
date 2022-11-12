2e5b947198682e1a1eddd3c0bac8c5306f5ea516
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-12 15:24:38 +0000

    Sat, Nov 12, 2022, 3:24 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,17 +3,16 @@ import sys
 import random
 
 figlet = Figlet()
-if len(sys.argv)==0:
+if len(sys.argv)==1:
     user_input=print("Input:")
     rand=figlet.getFonts()
     output=random.choice(rand)
     print(figlet.renderText(user_input , font=rand))
-elif len(sys.argv)==2:
+elif len(sys.argv)==3 and (sys.argv[1]=="-f" or sys.argv[1]=="--font"):
     font_type=sys.argv[1]
     font_style=sys.argv[2]
-    if font_type[0:1]=="-f" or font_type[0:]=="--font":
-        user_input:print("Input:")
-        print(figlet.renderText(user_input, font=font_style))
-    else:
-        sys.exit()
+    user_input:print("Input:")
+    print(figlet.renderText(user_input, font=font_style))
+else:
+    sys.exit()
 
