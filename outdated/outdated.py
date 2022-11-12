015b58b4b40edd27b4b2429674055e0c53619b37
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-12 18:36:24 +0000

    Sat, Nov 12, 2022, 6:36 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,7 +15,7 @@ month=[
 while True:
     date=input("Date:").title()
     try:
-        m, d,y=date.split("/")
+        m,d,y=date.split("/")
         if (1>=int(m)<=12 and 1>=int(d)<=31):
             break
     except:
