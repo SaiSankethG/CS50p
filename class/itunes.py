9b7a53365d67773870eb0a6223bb7f378a84a1de
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-12 01:24:13 +0000

    Sat, Nov 12, 2022, 1:24 AM +00:00

--- class/itunes.py
+++ class/itunes.py
@@ -1,4 +1,5 @@
+import json
 import requests
 import sys
 response=requests.get("https://itunes.apple.com/search?entity&limit=1&term=" + sys.argv[1])
-print(response)
+print(json.dumps(response.json(), indent=2))
