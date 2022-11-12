23695ec463c2c6559aa15a51637eacac13726318
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-12 01:30:40 +0000

    Sat, Nov 12, 2022, 1:30 AM +00:00

--- class/itunes.py
+++ class/itunes.py
@@ -1,5 +1,7 @@
 import json
 import requests
 import sys
-response=requests.get("https://itunes.apple.com/search?entity&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+response=requests.get("https://itunes.apple.com/search?entity&limit=50&term=" + sys.argv[1])
+o=response.json()
+for i in o["results"]:
+    print(i["trackName"])
