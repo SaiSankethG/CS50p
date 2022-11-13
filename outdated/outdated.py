f6a183c6c08c30ce4b4567a9f650029634b8d5ed
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-13 04:05:57 +0000

    Sun, Nov 13, 2022, 4:05 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,4 +18,10 @@ while True:
     if "/" in date:
         month , day, year=date.split("/")
         if 1<=int(month)<=12 and 1<=int(day)<=31:
-            
+            pass
+        else:
+            break
+    elif "," in date:
+        month , o_day, year=date.split(",")
+        day=o_day.replace("," , "")
+        
