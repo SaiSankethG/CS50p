ce3f4f5b4d3b8c56b267ac39e0a2eeea61600d9f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-13 03:49:28 +0000

    Sun, Nov 13, 2022, 3:49 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,16 +15,19 @@ months = [
 while True:
     date=input("date:")
     if "/" in date:
-        month , day ,year=date.split("/")
-        break
+        if int(month)>12 and int(day)>31:
+            continue
+        else:
+            month , day ,year=date.split("/")
+            break
     elif "," in date:
         date=date.replace("," , "")
         month, day, year=date.split(" ")
         if month in months:
             month=months.index(month)+1
-            break
-    if int(month)>12 and int(day)>31:
-        continue
-    else:
-        break
+            if int(month)>12 and int(day)>31:
+                continue
+            else:
+                break
+
 print(f"{int(year)}-{int(month):02}-{int(day):02}")
