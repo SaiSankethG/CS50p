24bf83c535c6dd7650b397f7e6e4bd4b5e446036
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-15 14:05:26 +0000

    Tue, Nov 15, 2022, 2:05 PM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -6,4 +6,8 @@ while True:
     except:
         print()
         break
-if 
+if "liesl" in name:
+    print("Adieu, adieu, to Liesl")
+elif "Friedrich" and "Liesl" in name:
+    print("Adieu, adieu, to Liesl and Friedrich")
+elif ""
