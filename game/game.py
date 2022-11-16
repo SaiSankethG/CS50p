f6c4c38553cdb9191a622d9410afe831077d8ea3
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-16 13:26:25 +0000

    Wed, Nov 16, 2022, 1:26 PM +00:00

--- game/game.py
+++ game/game.py
@@ -1,12 +1,23 @@
-import random:
+import random
 
 while True:
     level=input("Level: ")
     try:
         if int(level)>0:
             break
-    except ValueError:
+    except (ValueError ,IndexError):
         pass
 num=random.choice(range(0,))
 while True:
-    
+    number=input("Guess:")
+    try:
+        if(number<num):
+            print("Too small!")
+            continue
+        elif(number>num):
+            print("Too large!")
+            continue
+        elif number==num:
+            print("Just right!")
+    except():
+        pass
