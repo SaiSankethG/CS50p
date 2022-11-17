cd7093b35a768becd89d2705d13dd6773148466a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-17 00:36:30 +0000

    Thu, Nov 17, 2022, 12:36 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,11 +2,8 @@ import sys
 import requests
 import json
 
-response=requests.get( "https://api.coindesk.com/v1/bpi/currentprice.json")
-r=response.json()
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+r = response.json()
 for i in r["bpi"]:
     for j in i["USD"]:
         print(j["rate"])
-
-
-
