0033d27a7ff3cbf23546a8ef09b732d4f832998e
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-17 00:33:50 +0000

    Thu, Nov 17, 2022, 12:33 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,5 +4,9 @@ import json
 
 response=requests.get( "https://api.coindesk.com/v1/bpi/currentprice.json")
 r=response.json()
-for i in r["bpi"]
+for i in r["bpi"]:
+    for j in i["USD"]:
+        
+
+
 
