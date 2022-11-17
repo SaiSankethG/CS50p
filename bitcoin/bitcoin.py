6c43a6f47797936799e5d85990aaf9eeaa3d6ba3
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-17 00:40:10 +0000

    Thu, Nov 17, 2022, 12:40 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,6 +2,12 @@ import sys
 import requests
 import json
 
+if len(sys.argv)==2:
+    
+else:
+    print("Missing command-line argument")
+    sys.exit()
+
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 r = response.json()
 for i in r["bpi"]:
