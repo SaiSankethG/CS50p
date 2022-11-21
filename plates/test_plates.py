097155238b7b387a2265054cffc69adab659e31c
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-21 14:58:59 +0000

    Mon, Nov 21, 2022, 2:58 PM +00:00

---
+++ plates/test_plates.py
@@ -0,0 +1,17 @@
+from plates.plates import is_valid
+
+def main():
+    test_valid
+    test_invalid
+
+def test_valid():
+    assert is_valid("CS50")==True'
+
+def test_invalid():
+    assert is_valid('CS05')=='False'
+    assert is_valid('CS50p')=='False'
+    assert is_valid('H')=="False"
+    assert is_valid('outatime')=='False'
+
+if __name__=="__main__":
+    main()
