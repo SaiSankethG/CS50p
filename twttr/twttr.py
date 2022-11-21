3d0b36934d9e4aa977336c10898a092a3c4e37ac
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-21 00:58:41 +0000

    Mon, Nov 21, 2022, 12:58 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,18 +1,23 @@
 def main():
     word=input("Input: ")
-    shorten(word)
+    word=shorten(word)
     print("Output: " , end="")
+    print(word ,end="")
     print()
 
 def shorten(word):
+    short=NULL
     for i in word:
         if i>="A" and i<="Z":
             if i in ["A" , "E" , "I" , "O" , "U" ]:
                 continue
             else:
-                
+                short=i+short
         else :
             if i in ["a" , "e" , "i" , "o" , "u" ]:
                 continue
             else:
-                print(i , end="")
+                short=i+short
+    return short
+if __name__=="__main__":
+    main()
