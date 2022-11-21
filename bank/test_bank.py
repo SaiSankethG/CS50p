cccdc4c104850a3e75271377a9ba7f065eceeafe
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-21 13:27:28 +0000

    Mon, Nov 21, 2022, 1:27 PM +00:00

--- bank/test_bank.py
+++
@@ -1,8 +0,0 @@
-import bank
-
-def test_bank():
-    assert value(hello)=="$20"
-    assert value(hi)=="$0"
-    assert value(wtf)=="$100"
-
-
---
+++ test_bank/test_bank.py
@@ -0,0 +1,9 @@
+from bank.bank import value
+
+def main():
+    test_0
+
+
+def test_0():
+    
+
