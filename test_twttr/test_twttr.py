ab82102bf6e6f51bb79137a8ebc846897e37268c
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-21 12:59:42 +0000

    Mon, Nov 21, 2022, 12:59 PM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -3,5 +3,10 @@ from twttr.twttr import shorten
 def test_uppercase():
     assert shorten('LONG')=='LNG'
     assert shorten('TWITTER')=='TWTTR'
+
+def test_number():
     assert shorten('1234')=='1234'
+
+def test_lowercase():
+    assert shorten('twitter')=='twttr'
 
