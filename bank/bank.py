d667ce498ffa74ea8d0310b130d6232b17038212
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-21 13:32:26 +0000

    Mon, Nov 21, 2022, 1:32 PM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -5,12 +5,13 @@ def main():
    print(dollar)
 
 def value(greeting):
+   greeting=greeting.lower()
    if greeting.split()[0].replace("," , "")=="hello":
       return "$0"
    elif greeting[0]=="h" and greeting.split()[0]!="hello":
       return ("$20")
    else:
       return ("$100")
-   
+
 if __name__=="__main_":
    main()
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,8 +2,20 @@ from bank.bank import value
 
 def main():
     test_0
+    test_20
+    test_100
 
 
 def test_0():
-    
+    assert value('hello')=='$0'
+    assert value('HELLO')=='$0'
+def test_20():
+    assert value('hi')=='$20'
+    assert value('HI')=='$20'
+def test_100():
+    assert value('wtf')=='$100'
+    assert value('what the fuck')=='$100'
+
+if __name__=="__main__":
+    main()
 
