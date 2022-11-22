bc068f1e5756b0ab284b70d07acec68bd6fd12c6
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-22 00:47:55 +0000

    Tue, Nov 22, 2022, 12:47 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,10 +1,10 @@
 from fuel.fuel import convert, gauge
 
 def test_convert():
-    convert(1/10)==0.1
-    convert(1/1)==1
-    convert(1)=="ValueError"
+    assert convert('1/10')==0.1
+    assert convert('1/1')==1
+    assert convert('1')=="ValueError"
 
 def test_gauge():
-    gauge(0.1)==10
-    gauge(1)=='E'
+    assert gauge(0.1)==10
+    assert gauge(1)=='E'
