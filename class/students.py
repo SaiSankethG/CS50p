c67629132a58cdda0ee30296a48ce4f5afea833e
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-23 15:04:50 +0000

    Wed, Nov 23, 2022, 3:04 PM +00:00

--- class/students.py
+++ class/students.py
@@ -12,5 +12,8 @@ with open("names.csv") as file:
         student={"name":name , "college":college}
         students.append(student)
 
-for student in students:
+def getname(student):
+    return student["college"]
+
+for student in sorted(students, key=getname):
     print(f"{student['name']} is in {student['college']}")
