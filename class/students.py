4ccb652fb94d9f59967cf16e7cdd4077f4425b56
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-23 15:01:56 +0000

    Wed, Nov 23, 2022, 3:01 PM +00:00

--- class/students.py
+++ class/students.py
@@ -9,7 +9,8 @@ with open("names.csv") as file:
     for line in file:
         name, college=line.rstrip().split(",")
         student={}
-        student["name"]=name
-        student["house"]=house
+        student={"name":name , "college":college}
         students.append(student)
 
+for student in students:
+    print(f"{student['name']} is in {student['college']}")
