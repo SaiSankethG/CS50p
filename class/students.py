e8757a3e8b78e73dc50546fbdf6c93647e58d42d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-23 23:33:59 +0000

    Wed, Nov 23, 2022, 11:33 PM +00:00

--- class/students.py
+++ class/students.py
@@ -2,15 +2,15 @@
 #     for line in file:
 #         name , college=line.rstrip().split(",")
 #         print(f"{name} is in {college}")
-
+import csv
 students=[]
 
 with open("names.csv") as file:
-    for line in file:
-        name, college=line.rstrip().split(",")
-        student={}
-        student={"name":name , "college":college}
-        students.append(student)
-
+    # for line in file:
+    #     name, college=line.rstrip().split(",")
+    #     student={}
+    #     student={"name":name , "college":college}
+    #     students.append(student)
+    
 for student in sorted(students, key=lambda student:student["name"]):
     print(f"{student['name']} is in {student['college']}")
