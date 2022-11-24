551c0ed927d5e850e1f6272e9c794fecf352b031
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-24 00:41:19 +0000

    Thu, Nov 24, 2022, 12:41 AM +00:00

--- class/costumes.py
+++ class/costumes.py
@@ -1,2 +1,10 @@
 import sys
 from PIL import Image
+images=[]
+for arg in sys.arg[1:]:
+    image=image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "costumes.gif" , save_all=True , append_images=[images[1]] , duration=200 , loop=0
+)
