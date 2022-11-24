3e31f88c7fee90bc70d549afc7d16bc7d61fe668
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-24 00:17:40 +0000

    Thu, Nov 24, 2022, 12:17 AM +00:00

--- class/students.py
+++ class/students.py
@@ -2,17 +2,23 @@
 #     for line in file:
 #         name , college=line.rstrip().split(",")
 #         print(f"{name} is in {college}")
-import csv
-students=[]
+# import csv
+# students=[]
 
-with open("names.csv") as file:
+# with open("names.csv") as file:
     # for line in file:
     #     name, college=line.rstrip().split(",")
     #     student={}
     #     student={"name":name , "college":college}
     #     students.append(student)
-    read=csv.DictReader(file)
-    for row in read:
-        students.append({"name":row["name"] , "home":row["home"]})
-for student in sorted(students, key=lambda student:student["home"]):
-    print(f"{student['name']} is from {student['home']}")
+#     read=csv.DictReader(file)
+#     for row in read:
+#         students.append({"name":row["name"] , "home":row["home"]})
+# for student in sorted(students, key=lambda student:student["home"]):
+#     print(f"{student['name']} is from {student['home']}")
+import csv
+
+name=input("whats your name?")
+home=input("Where's your name?")
+with open("names.csv" ,"w") as file:
+    
