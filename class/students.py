48b7338a657435467894eba66e1a032b6d23f559
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-24 00:24:47 +0000

    Thu, Nov 24, 2022, 12:24 AM +00:00

--- class/students.py
+++ class/students.py
@@ -21,5 +21,5 @@ import csv
 name=input("whats your name?")
 home=input("Where's your name?")
 with open("names.csv" ,"a") as file:
-    writer=csv.DictWriter(file)
-    writer.writerow([name, home])
+    writer=csv.DictWriter(file ,fieldnames=["name" ,"home"])
+    writer.writerow({"name":name , "home":home})
