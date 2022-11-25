d156b62664acfb7715f1cf04d49f0c1cac14bf87
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-25 07:34:59 +0000

    Fri, Nov 25, 2022, 7:34 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -17,14 +17,10 @@ def table():
         if "regular.csv" in sys.argv[1]:
             with open("regular.csv" , "r") as file:
                 tab=csv.DictReader(file , fieldnames=["name" , "small" , "large"])
-                # for t in tab:
-                #     print(t["name"] , t["small"] , t["large"])
                 print(tabulate(tab,headers="firstrow", tablefmt="grid"))
         if "sicilian.csv" in sys.argv[1]:
             with open("sicilian.csv" , "r") as file:
                 tab=csv.DictReader(file , fieldnames=["name" , "small" , "large"])
-                # for t in tab:
-                #     print(t["name"] , t["small"] , t["large"])
                 print(tabulate(tab,headers="firstrow", tablefmt="grid"))
     except FileNotFoundError:
         sys.exit("File does not exist")
