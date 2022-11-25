9166aa468d84f59346fb66e7690f5fc7d7578489
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-25 02:24:40 +0000

    Fri, Nov 25, 2022, 2:24 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,19 +1,14 @@
 import sys
 def main():
     try:
-        try:
-            if sys.argv[0]==0:
-                print(end="")
-        except IndexError:
-            print("Too few command-line arguments")
-            sys.exit(0)
+        if len(sys.argv)<2:
+            sys.exit("Too few command-line arguments")
         if len(sys.argv)>2:
             print("Too many command-line arguments")
             sys.exit(0)
-        elif ".py" not in sys.argv[1]:
-            print("Not a python file")
-            sys.exit(0)
-        elif ".py" in sys.argv[1]:
+        if ".py" not in sys.argv[1]:
+            sys.exit("Not a python file")
+        if ".py" in sys.argv[1]:
             lines_of_code()
     except FileNotFoundError:
         print("File not Found")
