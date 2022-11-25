3d17e826ce9e80b8fe5ac1c6afdd538bd90e061a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-25 05:55:07 +0000

    Fri, Nov 25, 2022, 5:55 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,4 +3,9 @@ import csv
 
 def main():
     if len(sys.argv)<2:
-        sys.exit("")
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv)>2:
+        sys.exit("Too many command-line arguments")
+    if ".py" not in sys.argv[1]:
+        sys.exit("Not a CSV file")
+    if 
