8b7a73e6264030a163f2cce3436d571ac7d1f8c5
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-25 15:14:54 +0000

    Fri, Nov 25, 2022, 3:14 PM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,4 +5,9 @@ def main():
     command_line_arguments()
 
 def command_line_arguments():
-    
+    if len(sys.argv)<1:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv)>3:
+        sys.exit("Too many command-line arguments")
+    if ".csv" not in sys.argv[1]:
+        print()
