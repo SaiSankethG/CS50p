6d7b76bb547dab577247ae495a0b47c80bebaf11
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-25 06:17:19 +0000

    Fri, Nov 25, 2022, 6:17 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,7 +7,7 @@ def main():
         sys.exit("Too few command-line arguments")
     if len(sys.argv)>2:
         sys.exit("Too many command-line arguments")
-    if ".py" not in sys.argv[1]:
+    if ".csv" not in sys.argv[1]:
         sys.exit("Not a CSV file")
     if ".csv" in sys.argv[1]:
         table()
@@ -16,7 +16,7 @@ def table():
     if "regular.csv" in sys.argv[1]:
         with open("regular.csv" , "r") as file:
             tab=csv.DictWriter(file , fieldnames=["name" , "small" , "large"])
-            print(tabulate(tab , header , tablefmt="grid"))
+            print(tabulate(tab, tablefmt="grid"))
 
 if __name__=="__main__":
     main()
