efb00d8e34b30743c6d6861d6df0991d59ede186
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-26 02:32:54 +0000

    Sat, Nov 26, 2022, 2:32 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,10 +2,13 @@ import sys
 from PIL import image
 def main():
     validate()
+    try:
+        
 
 def validate():
     if len(sys.argv)<3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv)>3:
         sys.exit("Too many command-line arguments")
-    if 
+    if sys.argv[1].split(".") != sys.argv[2].split("."):
+        sys.exit("Input and output have different extensions")
