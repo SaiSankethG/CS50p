d3057ab71c49a7f51ef8d85445a00b01ed125ee0
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-26 02:40:27 +0000

    Sat, Nov 26, 2022, 2:40 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -10,6 +10,8 @@ def validate():
         sys.exit("Too few command-line arguments")
     if len(sys.argv)>3:
         sys.exit("Too many command-line arguments")
+    if ".png"and".jpg"and".jpeg" not in sys.argv[1] and sys.argv[2]:
+        sys.exit("Invalid input")
     if sys.argv[1].split(".") != sys.argv[2].split("."):
         sys.exit("Input and output have different extensions")
 if __name__=="__main__":
