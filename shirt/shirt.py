be70e6b1d4cf2677d468da66b009984b44a26750
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-26 02:29:18 +0000

    Sat, Nov 26, 2022, 2:29 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,6 +5,7 @@ def main():
 
 def validate():
     if len(sys.argv)<3:
-        sys.exit("Too few command line arguments")
+        sys.exit("Too few command-line arguments")
     if len(sys.argv)>3:
-        
+        sys.exit("Too many command-line arguments")
+    if 
