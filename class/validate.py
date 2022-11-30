220bbe31a6ab2ef870e6387c25dc9771e29773ae
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-30 11:35:23 +0000

    Wed, Nov 30, 2022, 11:35 AM +00:00

--- class/validate.py
+++ class/validate.py
@@ -1,8 +1,14 @@
+import re
+
 email=input("whats your email?").strip()
 
-username , domain=email.split("@")
+# username , domain=email.split("@")
 
-if username and "." in domain:
+# if username and "." in domain:
+#     print("Valid")
+# else:
+#     print("Invalid")
+if re.search("@" , email):
     print("Valid")
 else:
     print("Invalid")
