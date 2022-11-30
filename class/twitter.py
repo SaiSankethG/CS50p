20972348dfd86ab007baf8e6dcd776da462c5bfb
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-11-30 18:37:34 +0000

    Wed, Nov 30, 2022, 6:37 PM +00:00

--- class/twitter.py
+++ class/twitter.py
@@ -1,3 +1,6 @@
+import re
+
 url=input("URL:").strip()
-username=url.replace("https://twitter.com/" , "")
-print(f"Username:{username}")
+
+name=re.sub(r"https://twitter.com/" , "" , url)
+print(f"Name:{name}")
