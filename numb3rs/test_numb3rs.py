275822f870e788573adcf74edf64458619203735
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-02 13:57:26 +0000

    Fri, Dec 2, 2022, 1:57 PM +00:00

---
+++ numb3rs/test_numb3rs.py
@@ -0,0 +1,19 @@
+import numb3rs
+
+def main():
+    test_char()
+    test_out_of_range()
+    test_range()
+
+def test_char():
+    assert validate(cat)==False
+    assert validate(dog)==False
+def test_out_of_range():
+    assert validate("277.0.0.1")==False
+    assert validate("255.255.255.288")==False
+    assert validate("127.0.0.1111")==False
+def test_range():
+    assert validate("127.0.0.1")==True
+    assert validate("124.0.0.1")==True
+if __name__=="__main__":
+    main()
