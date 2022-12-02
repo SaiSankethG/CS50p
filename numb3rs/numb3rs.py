ef208d3a438c09b7c06de5e68cb966234f623121
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-02 13:43:15 +0000

    Fri, Dec 2, 2022, 1:43 PM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,7 +10,7 @@ def validate(ip):
     if re.search(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" , ip):
         list_of_numbers=ip.split(".")
         for number in list_of_numbers:
-            if number>0 or number<255:
+            if int(number)>0 or int(number)<255:
                 return True
             else:
                 return False
