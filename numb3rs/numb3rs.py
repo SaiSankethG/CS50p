d99b34cb65e511e23893a00f985e96fd46e22e6f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-02 13:32:01 +0000

    Fri, Dec 2, 2022, 1:32 PM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    matches=re.search(r"[0-255]\.[0-255]\.[0-255]\.[0-255]" , ip)
+    matches=re.search(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" , ip)
     if matches:
         return True
     else:
