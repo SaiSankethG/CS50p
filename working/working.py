6acb3d8086ea84f705cb786a250c94eb02153a6f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-03 17:25:57 +0000

    Sat, Dec 3, 2022, 5:25 PM +00:00

--- working/working.py
+++ working/working.py
@@ -7,11 +7,19 @@ def main():
 
 
 def convert(s):
-    try :
-        if re.search(r"^([0-9]\.[0-9][0-9])(.*to.*)([0-9]\.[0-9][0-9]).*$" , s):
-            if 
-    except ValueError:
-        print("It is not valid")
+    # try :
+    #     if re.search(r"^([0-9]\.[0-9][0-9])(.*to.*)([0-9]\.[0-9][0-9]).*$" , s):
+    #         if
+    # except ValueError:
+    #     print("It is not valid")
+    #def convert(time):
+    hour, minute, meridian = time.split(" ")
+    hour = int(hour)
+    if meridian == "PM" and hour != 12:
+        hour += 12
+    elif meridian == "AM" and hour == 12:
+        hour = 0
+    return "{:02d}:{:02d}".format(hour, int(minute))
 
 
 
