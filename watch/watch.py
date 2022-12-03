3befb74146d40b8bce41fbca73a2922eb5916b64
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-03 18:33:52 +0000

    Sat, Dec 3, 2022, 6:33 PM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -3,15 +3,30 @@ import sys
 
 
 def main():
-    print(parse(input("HTML: ")))
+    print(convert(input("Hours: ")))
 
 
-def parse(s):
-    if re.search(r"<iframe.*><\/iframe>" , s):
-        url=re.search(r"(https?:\/\/(www\.)?youtube\.com\/embed\/)([A-Za-z0-9]+)" ,s)
-        if url:
-            split_url=url.groups()
-            return "https://youtu.be/" + split_url[2]
+def convert(s):
+    if user_input:=re.search(r"^([0-9]+)(\:[0-9]+)?\ (AM|PM)\ to\ ([0-9]+)(\:[0-9]+)?\ (AM|PM)$", s):
+        time=list(user_input.groups())
+        # print(time)
+        # print(time[0])
+        time[1]=":00"
+        time[4]=":00"
+        if time[2] == "AM" or "PM":
+            if time[2] == "AM":
+                n=time[0]
+                print(f"{n:02}" , time[1] , " to " ,sep='' , end='')
+            elif time[2] == "PM":
+                time[0]=int(time[0])+12
+                print(time[0] , time[1] , " to " , sep='' , end='')
+        if time[5]==  "AM" or "PM":
+            if time[5] == "AM":
+                m=time[3]
+                print(f"{m:02}" , time[4],sep='')
+            elif time[5] == "PM":
+                time[3]=int(time[3])+12
+                print(time[3] , time[4] , sep='')
 
 if __name__ == "__main__":
     main()
