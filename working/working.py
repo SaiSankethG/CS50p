6838e6a269ac79b5c36069bdf1670fea7ce044a1
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-03 16:54:31 +0000

    Sat, Dec 3, 2022, 4:54 PM +00:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,12 @@ def main():
 
 
 def convert(s):
-    
+    try :
+        if re.search(r"^([0-9]\.[0-9][0-9])(.*to.*)([0-9]\.[0-9][0-9]).*$" , s):
+            
+    except ValueError:
+        print("It is not valid")
+
 
 
 if __name__ == "__main__":
