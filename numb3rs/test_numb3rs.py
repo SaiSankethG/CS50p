765be65fbcf047d12e37c1bd0bc429276cd1d850
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-03 01:45:46 +0000

    Sat, Dec 3, 2022, 1:45 AM +00:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -8,14 +8,14 @@ def main():
     sys.exit(0)
 
 def test_char():
-    assert validate("cat")==False
-    assert validate("dog")==False
+    assert validate(r"cat")==False
+    assert validate(r"dog")==False
 def test_out_of_range():
-    assert validate("277.0.0.1")==False
-    assert validate("255.255.255.288")==False
-    assert validate("127.0.0.1111")==False
+    assert validate(r"277.0.0.1")==False
+    assert validate(r"255.255.255.288")==False
+    assert validate(r"127.0.0.1111")==False
 def test_range():
-    assert validate("127.0.0.1")==True
-    assert validate("124.0.0.1")==True
+    assert validate(r"127.0.0.1")==True
+    assert validate(r"124.0.0.1")==True
 if __name__=="__main__":
     main()
