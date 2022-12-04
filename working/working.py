b2b4a66e88981bbc56b5ab289146dffae56100dd
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 08:24:15 +0000

    Sun, Dec 4, 2022, 8:24 AM +00:00

--- working/working.py
+++ working/working.py
@@ -3,32 +3,35 @@ import sys
 
 
 def main():
-    print(convert(input("Hours: ")))
+    convert(input("Hours: "))
 
 
 def convert(s):
-    if user_input:=re.search(r"^([0-9]+)(\:[0-9]+)?\ (PM|AM)\ to\ ([0-9]+)(\:[0-9]+)?\ (AM|PM)$", s):
+    if user_input:=re.search(r"^([0-9]+)\:?([0-9]+)?\ (PM|AM)\ to\ ([0-9]+)\:?([0-9]+)?\ (AM|PM)$", s):
         time=list(user_input.groups())
         #print(time)
         if int(time[0])<12 and int(time[3])<12:
             if time[1]==None:
-                time[1]=":00"
-                time[4]=":00"
+                time[1]="00"
+                time[4]="00"
                 #print(time[1] , time[4])
-            if time[2] == "AM" or "PM":
-                if time[2] == "AM":
-                    n=int(time[0])
-                    print(f"{n:02}", time[1] , " to " ,sep='' , end='')
-                elif time[2] == "PM":
-                    time[0]=int(time[0])+12
-                    print(time[0] , time[1] , " to " , sep='' , end='')
-            if time[5]==  "AM" or "PM":
-                if time[5] == "AM":
-                    m=int(time[3])
-                    print(f"{m:02}" , time[4],sep='')
-                elif time[5] == "PM":
-                    time[3]=int(time[3])+12
-                    print(time[3] , time[4] , sep='')
+            if 0<=int(time[1])<60 and 0<=int(time[4])<60:
+                if time[2] == "AM" or "PM":
+                    if time[2] == "AM":
+                        n=int(time[0])
+                        print(f"{n:02}", f":{time[1]}" , " to " ,sep='' , end='')
+                    elif time[2] == "PM":
+                        time[0]=int(time[0])+12
+                        print(time[0] , f":{time[1]}" , " to " , sep='' , end='')
+                if time[5]==  "AM" or "PM":
+                    if time[5] == "AM":
+                        m=int(time[3])
+                        print(f"{m:02}" ,f":{time[4]}",sep='')
+                    elif time[5] == "PM":
+                        time[3]=int(time[3])+12
+                        print(time[3] , f":{time[4]}" , sep='')
+            else:
+                raise ValueError
         else:
             raise ValueError
     else:
