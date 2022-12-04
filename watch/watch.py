1ed91dcd56ca833dfa021973466ccd710495c0e3
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 05:30:19 +0000

    Sun, Dec 4, 2022, 5:30 AM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,31 +7,28 @@ def main():
 
 
 def convert(s):
-    if user_input:=re.search(r"^([0-9]+)(\:[0-9]+)?\ (PM|AM)\ to\ ([0-9]+)(\:[0-9]+)?\ (AM|PM)$", s):
-        time=list(user_input.groups())
-        print(time)
-        if int(time[0])<12 and int(time[3])<12:
-            if time[1]==None:
-                time[1]=":00"
-                time[4]=":00"
-                print(time[1] , time[4])
+    try:
+        if user_input:=re.search(r"^([0-9]+)(\:[0-9]+)?\ (AM|PM)\ to\ ([0-9]+)(\:[0-9]+)?\ (AM|PM)$", s):
+            time=list(user_input.groups())
+            # print(time)
+            # print(time[0])
+            time[1]=":00"
+            time[4]=":00"
             if time[2] == "AM" or "PM":
                 if time[2] == "AM":
-                    n=time[0]
-                    print(time[0] , time[1] , " to " ,sep='' , end='')
+                    
+                    print(f"{time[0]:02}" , time[1] , " to " ,sep='' , end='')
                 elif time[2] == "PM":
                     time[0]=int(time[0])+12
                     print(time[0] , time[1] , " to " , sep='' , end='')
             if time[5]==  "AM" or "PM":
                 if time[5] == "AM":
-                    print(time[3] , time[4],sep='')
+                    print(f"{time[3]:02}" , time[4],sep='')
                 elif time[5] == "PM":
                     time[3]=int(time[3])+12
                     print(time[3] , time[4] , sep='')
-        else:
-            raise ValueError
-    else:
-        raise ValueError
+    except ValueError:
+        print("It is not valid")
 
 if __name__ == "__main__":
     main()
--- working/working.py
+++ working/working.py
@@ -6,22 +6,29 @@ def main():
     print(convert(input("Hours: ")))
 
 
-def convert(time):
-    # try :
-    #     if re.search(r"^([0-9]\.[0-9][0-9])(.*to.*)([0-9]\.[0-9][0-9]).*$" , s):
-    #         if
-    # except ValueError:
-    #     print("It is not valid")
-    #def convert(time):
-    hour, minute, meridian = time.split(" ")
-    hour = int(hour)
-    if meridian == "PM" and hour != 12:
-        hour += 12
-    elif meridian == "AM" and hour == 12:
-        hour = 0
-    return "{:02d}:{:02d}".format(hour, int(minute))
+def convert(s):
+    try:
+        if user_input:=re.search(r"^([0-9]+)(\:[0-9]+)?\ (AM|PM)\ to\ ([0-9]+)(\:[0-9]+)?\ (AM|PM)$", s):
+            time=list(user_input.groups())
+            # print(time)
+            # print(time[0])
+            time[1]=":00"
+            time[4]=":00"
+            if time[2] == "AM" or "PM":
+                if time[2] == "AM":
 
-
+                    print(f"{time[0]:02}" , time[1] , " to " ,sep='' , end='')
+                elif time[2] == "PM":
+                    time[0]=int(time[0])+12
+                    print(time[0] , time[1] , " to " , sep='' , end='')
+            if time[5]==  "AM" or "PM":
+                if time[5] == "AM":
+                    print(f"{time[3]:02}" , time[4],sep='')
+                elif time[5] == "PM":
+                    time[3]=int(time[3])+12
+                    print(time[3] , time[4] , sep='')
+    except ValueError:
+        print("It is not valid")
 
 if __name__ == "__main__":
     main()
