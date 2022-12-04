092e6d0ac9a4f0d6043f5d1350c1ec202cb60f6e
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 11:51:14 +0000

    Sun, Dec 4, 2022, 11:51 AM +00:00

--- um/um.py
+++ um/um.py
@@ -7,12 +7,15 @@ def main():
 
 
 def count(s):
-    um = re.compile(r"um[^\w]+")
-    count_um = 0
-    for u in um.finditer(s):
-        if u == "um":
-            count_um += 1
-    return count_um
+    # um = re.compile(r"um[^\w]+")
+    # count_um = 0
+    # for u in um.finditer(s):
+    #     if u == "um":
+    #         count_um += 1
+    # return count_um
+    um=re.findall( r"um[^\w]*" , s)
+    count=0
+    print(um)
 
 if __name__ == "__main__":
     main()
