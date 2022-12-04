3727eb685cee2fb7e318e15a524fb5a1ddb44e3f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 11:56:05 +0000

    Sun, Dec 4, 2022, 11:56 AM +00:00

--- um/um.py
+++ um/um.py
@@ -13,9 +13,11 @@ def count(s):
     #     if u == "um":
     #         count_um += 1
     # return count_um
-    um=re.findall( r"[^\w]*um[^\w]*" , s)
-    for all_um in um:
-        if all_um == "um":
-            
+    um=re.findall( r"[^a-z]um[^a-z]" , s)
+    print(um)
+    # for all_um in um:
+    #     if all_um == "um":
+
+
 if __name__ == "__main__":
     main()
