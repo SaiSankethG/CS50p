c750de239200bc06f77f430ea1dd7ebe9f61d34f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 08:53:46 +0000

    Sun, Dec 4, 2022, 8:53 AM +00:00

--- working/working.py
+++ working/working.py
@@ -7,35 +7,24 @@ def main():
 
 
 def convert(s):
-    if user_input:=re.search(r"^([0-9]+)\:?([0-9]+)?\ (PM|AM)\ to\ ([0-9]+)\:?([0-9]+)?\ (AM|PM)$", s):
-        time=list(user_input.groups())
-        #print(time)
-        if int(time[0])<12 and int(time[3])<12:
-            if time[1]==None:
-                time[1]="00"
-                time[4]="00"
-                #print(time[1] , time[4])
-            if 0<=int(time[1])<60 and 0<=int(time[4])<60:
-                if time[2] == "AM" or "PM":
-                    if time[2] == "AM":
-                        n=int(time[0])
-                        print(f"{n:02}", f":{time[1]}" , " to " ,sep='' , end='')
-                    elif time[2] == "PM":
-                        time[0]=int(time[0])+12
-                        print(time[0] , f":{time[1]}" , " to " , sep='' , end='')
-                if time[5]==  "AM" or "PM":
-                    if time[5] == "AM":
-                        m=int(time[3])
-                        print(f"{m:02}" ,f":{time[4]}",sep='')
-                    elif time[5] == "PM":
-                        time[3]=int(time[3])+12
-                        print(time[3] , f":{time[4]}" , sep='')
-            else:
-                raise ValueError
-        else:
+    correct_format=re.search(r"^(([0-9][0-2]*):*([0-5][]0-9])*) ([A|P]M) to (([0-9][0-2]*):*([0-5][]0-9])*) ([A|P]M)$" , s)
+    if correct_format:
+        pieces=correct_format.groups()
+        if int(pieces[1])>12 or int(pieces[5])>12:
             raise ValueError
+        return pieces
     else:
         raise ValueError
+
+def new_format(hour , minute, am_pm):
+    if am_pm=="PM":
+        if int(hour)==12:
+            new_hour=12
+        else:
+            new_hour=int(hour)+12
+    else:
+        if int(hour)
+
 
 if __name__ == "__main__":
     main()
