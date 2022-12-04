fab9a2139d8a4373c9e45bcf626204f09f0ac319
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 13:35:35 +0000

    Sun, Dec 4, 2022, 1:35 PM +00:00

--- response/response.py
+++ response/response.py
@@ -3,12 +3,6 @@ from validator_collection import validators, checkers, errors
 
 email=input("whats your email?").strip()
 
-# username , domain=email.split("@")
-
-# if username and "." in domain:
-#     print("Valid")
-# else:
-#     print("Invalid")
 if re.search(r"^\w+@(\w+\.)?\w+\.(edu)$" , email , re.IGNORECASE):
     print("Valid")
 else:
