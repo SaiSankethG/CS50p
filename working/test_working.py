d8fe6ff021b24a6ac49a06f9d8c446c788bad9ac
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 05:44:04 +0000

    Sun, Dec 4, 2022, 5:44 AM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,8 +1,17 @@
 from working.working import convert
 
 def main():
-    
+    test_valid()
+    test_valueerror()
 
+def test_valid():
+    assert convert("9 AM to 5 PM")=="09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM")=="09:00 to 17:00"
+    assert convert("10 PM to 8 AM")=="22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM")=="22:30 to 08:50"
+
+def test_valueerror():
+    
 
 if __name__=="__main__":
     main()
