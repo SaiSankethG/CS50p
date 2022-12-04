2d6ba98959223fc9faa987fae07a3bc4946855f6
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 13:29:52 +0000

    Sun, Dec 4, 2022, 1:29 PM +00:00

---
+++ response/response.py
@@ -0,0 +1,14 @@
+import re
+
+email=input("whats your email?").strip()
+
+# username , domain=email.split("@")
+
+# if username and "." in domain:
+#     print("Valid")
+# else:
+#     print("Invalid")
+if re.search(r"^\w+@(\w+\.)?\w+\.(edu)$" , email , re.IGNORECASE):
+    print("Valid")
+else:
+    print("Invalid")
