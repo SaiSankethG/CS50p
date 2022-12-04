098c158f246845f99a5c19fc2121f2c1f7af6cc3
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-04 13:06:42 +0000

    Sun, Dec 4, 2022, 1:06 PM +00:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,17 +1,29 @@
 from um.um import count
 
 def main():
-    test_value()
-    test_zero()
+#     test_value()
+#     test_zero()
+    test_upper_lower_case()
+    test_word_with_um()
+    test_by_space()
 
-def test_value():
-    assert count("hello, um, world , um")==2
-    assert count("um")==1
-    assert count("um?")==1
-    assert count("Um, thanks for the album")==1
+def test_upper_lower_case():
+    assert count("Um , thanks for the album")==1
+    assert count("Um , thanks , um...")==2
 
-def test_zero():
-    assert count("hello, world")==0
+def test_word_with_um():
+    assert count("yummy")==0
+
+def test_by_space():
+    assert count("Hello um world")==1
+# def test_value():
+#     assert count("hello, um, world , um")==2
+#     assert count("um")==1
+#     assert count("um?")==1
+#     assert count("Um, thanks for the album")==1
+
+# def test_zero():
+#     assert count("hello, world")==0
 
 if __name__=="__main__":
     main()
