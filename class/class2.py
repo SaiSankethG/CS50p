e7d883864ca0e62c7099af92a91f6d322d5d3604
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-05 13:48:35 +0000

    Mon, Dec 5, 2022, 1:48 PM +00:00

--- class/class2.py
+++ class/class2.py
@@ -7,11 +7,11 @@ class Student:
         self.name=name
         self.house=house
     def __str__(self):
-        
+        return f"{self.name} from {self.house}"
 
 def main():
     student=get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name=input("Name: ")
