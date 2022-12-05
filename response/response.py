ddc427fe9c15e2054c7039c67f1fb06ffbb82e4c
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-05 02:04:32 +0000

    Mon, Dec 5, 2022, 2:04 AM +00:00

--- response/response.py
+++ response/response.py
@@ -1,8 +1,13 @@
 from validator_collection import validators
 
-email=validators.email(input("What's your email address?") ,  allow_empty=False)
-if email:
+try:
+    email=validators.email(input("What's your email address?") ,  allow_empty=False)
     print("Valid")
-else:
+except (InvalidEmailError , ValueError):
     print("Invalid")
+
+# if email:
+#     print("Valid")
+# else:
+#     print("Invalid")
 #print(email)
