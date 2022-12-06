6096f4c7cb15f686bced86f74d7a0d58434ff60d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-06 01:54:46 +0000

    Tue, Dec 6, 2022, 1:54 AM +00:00

---
+++ class/hat.py
@@ -0,0 +1,10 @@
+import random
+
+class Hat():
+    def __init__(self):
+        self.houses=["Gryffindor" , "Slytherin" , "Hufflepuff" , "Ravenclaw"]
+    def sort(self , name):
+        print(f"{name} is in", random.choice(self.houses))
+
+hat=Hat()
+hat.sort("Harry")
