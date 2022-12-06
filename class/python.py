cd4d01b65f48abd11eaaa44bb885bcc9a1bb178f
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-06 13:13:41 +0000

    Tue, Dec 6, 2022, 1:13 PM +00:00

--- class/python.py
+++ class/python.py
@@ -1,15 +1,27 @@
 class Wizard:
-    
+    def __init__(self ,name):
+        self.name=name
+    def __str__(self):
+        return self.name
 
 class Student(Wizard):
     def __init__(self , name, house):
         super().__init__(name)
         self.house=house
+    def __str__(self):
+        return f"{self.name} is in {self.house}"
 
 
 class Professor(Wizard):
     def __init__(self , name , subject):
         super().__init__(name)
         self.subject=subject
+    def __str__(self):
+        return f"{self.name} subject is {self.subject}"
 
-
+wizard=Wizard("Albus")
+print(wizard)
+student=Student("Harry" , "Gryffindor")
+print(student)
+professor=Professor("Snape" ,"Defense aganist the dark arts")
+print(professor)
