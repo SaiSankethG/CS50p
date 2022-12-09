8da41a56d2a096b257c98ce3c7348d7a2dfb0ef0
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-09 12:25:38 +0000

    Fri, Dec 9, 2022, 12:25 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,13 +4,7 @@ import inflect
 
 class no_of_minutes:
     def __init__(self , user_date):
-        user_calendare
-        user_calendar=date.isocalendar(user_date)
-        print(user_calendar)
-
-
-        #print(year, month , day)
-    #def __sub__(self ,other):
+        
 
 
 
@@ -19,7 +13,7 @@ def main():
     user_date=input("Date of Birth: ")
     try:
         date.fromisoformat(user_date)
-        date_of_birth=no_of_minutes(user_date)
+        date_of_birth=no_of_minutes(date.isocalendar(user_date))
         today=no_of_minutes(date.today())
 
 
