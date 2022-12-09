15a4d612e749ff486b33fb181dd3972db535035b
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-09 12:02:41 +0000

    Fri, Dec 9, 2022, 12:02 PM +00:00

---
+++ class/vault.py
@@ -0,0 +1,13 @@
+class Vault:
+    def __init__(self , galleons=0, sickles=0, knuts=0):
+        self.galleons=galleons
+        self.sickles=sickles
+        self.knuts=knuts
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} sickles, {self.knuts} knuts"
+
+potter=Vault(100, 50 , 25)
+print(potter)
+
+weasly=Vault(25, 50, 100)
+print(weasly)
---
+++ seasons/seasons.py
@@ -0,0 +1,34 @@
+from datetime import date
+import inflect
+
+
+class no_of_minutes:
+    def __init__(self , user_date):
+        year, month, day=user_date.split("-")
+        self.year=year
+        self.month=month
+        self.day=day
+
+        #print(year, month , day)
+    def __sub__(self ,other):
+        
+
+
+
+def main():
+    user_date=input("Date of Birth: ")
+    try:
+        date.fromisoformat(user_date)
+        date_of_birth=no_of_minutes(user_date)
+        today=no_of_minutes(date.today())
+
+
+    except ValueError:
+        print("Invalid date")
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
