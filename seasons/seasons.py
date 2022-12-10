dfcb01caddcbf1999438f503489f1fd73428deb3
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-10 16:42:35 +0000

    Sat, Dec 10, 2022, 4:42 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -37,8 +37,9 @@ def main():
 
         #different in days , months, years
         diff_date=today_minutes-user_minutes
+        diff_minutes=diff_date.convert_minutes()
         #print(diff_date.convert_minutes())
-        
+        yea
 
 
     except ValueError:
