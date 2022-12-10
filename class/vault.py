04fe1a3456d6e53e7e49291c225df6132bb05d54
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-10 16:28:47 +0000

    Sat, Dec 10, 2022, 4:28 PM +00:00

--- class/vault.py
+++ class/vault.py
@@ -5,9 +5,16 @@ class Vault:
         self.knuts=knuts
     def __str__(self):
         return f"{self.galleons} Galleons, {self.sickles} sickles, {self.knuts} knuts"
+    def __add__(self ,other):
+        galleons=self.galleons+other.galleons
+        sickles=self.sickles+other.sickles
+        knuts=self.knuts+other.knuts
+        return Vault(galleons, sickles, knuts)
 
 potter=Vault(100, 50 , 25)
 print(potter)
 
-weasly=Vault(25, 50, 100)
+weasley=Vault(25, 50, 100)
 print(weasly)
+
+total=potter + weasley
