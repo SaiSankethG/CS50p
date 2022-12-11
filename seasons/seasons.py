4c39f47e7a7bb6a61a02770dd199f1843a74c0c7
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-11 02:51:16 +0000

    Sun, Dec 11, 2022, 2:51 AM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,9 +1,14 @@
 from datetime import date
 import sys
+import inflect
+
+p=inflect.engine()
+
 
 def main():
     user_date=input("Enter the date:")
     minutes=convert(user_date)
+    print(f"{p.number_to_words(minutes), andword=""} minutes")
 
 def convert(user_date):
     try:
@@ -11,8 +16,11 @@ def convert(user_date):
     except:
         sys.exit("Invalid Date")
     today=date.today()
-    days=today-b
-    minutes=days.total
+    sub=today-b
+    day=sub.days
+    total_minutes=day*1440
+    return round(total_minutes)
+
 
 
 ...
