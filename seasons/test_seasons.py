4582c28429f3e251d6a56de68b051bc60cb69856
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-11 03:37:47 +0000

    Sun, Dec 11, 2022, 3:37 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,11 +1,12 @@
 from seasons.seasons import convert
+import pytest
 
 def main():
     test_check_minutes()
 
 def test_check_minutes():
     assert convert("2003-09-01")=="ten million, one hundred thirty-nine thousand forty minutes"
-    with 
+    with pytest.
 
 if __name__=="__main__":
     main()
