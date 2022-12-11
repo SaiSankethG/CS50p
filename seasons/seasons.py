11845502750b156449112a0a8ebda3af43e56894
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-11 02:46:17 +0000

    Sun, Dec 11, 2022, 2:46 AM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,9 +1,18 @@
 from datetime import date
-
+import sys
 
 def main():
     user_date=input("Enter the date:")
-    
+    minutes=convert(user_date)
+
+def convert(user_date):
+    try:
+        b=date.fromisoformat(user_date)
+    except:
+        sys.exit("Invalid Date")
+    today=date.today()
+    days=today-b
+    minutes=days.total
 
 
 ...
