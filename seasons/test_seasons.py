3e69cad100d47e3a1bfa39eb7f399bd7c575c87a
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-11 03:36:03 +0000

    Sun, Dec 11, 2022, 3:36 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,11 +1,11 @@
 from seasons.seasons import convert
 
 def main():
-    check_minutes()
+    test_check_minutes()
 
-def check_minutes():
+def test_check_minutes():
     assert convert("2003-09-01")=="ten million, one hundred thirty-nine thousand forty minutes"
-    assert convert("01 september, 2003")==None
+    assert convert("01 september, 2003")=="Invalid Date"
 
 if __name__=="__main__":
     main()
