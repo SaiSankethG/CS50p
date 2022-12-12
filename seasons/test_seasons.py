03e369da490a17f3267d73aab17978671390264c
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2022-12-12 14:59:22 +0000

    Mon, Dec 12, 2022, 2:59 PM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -8,7 +8,8 @@ def test_check_minutes():
     assert convert("2003-09-01")=="ten million, one hundred forty thousand, four hundred eighty minutes"
     # with pytest.raise(ValueError):
     #     convert()
-    assert convert("")
+    assert convert("2021-12-12")=="five hundred twenty-five thousand, six hundred minutes"
+    assert convert(")
 
 if __name__=="__main__":
     main()
