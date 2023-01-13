200f2954ac8f33fd0e89e7efd862f16eeed7e929
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-13 17:22:35 +0000

    Fri, Jan 13, 2023, 5:22 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,9 +6,12 @@ class PDF():
     def __init__(self, name):
         self._pdf=FPDF()
         self._pdf.add_page()
-        self._pdf.set_font("helvetica", "B", 16)
+        self._pdf.set_font("helvetica", "B", 45)
         self._pdf.cell(0 , 60, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT" ,align="C")
-        # self._pdf.image()
+        self._pdf.image("shirtificate.png" ,w=self._pdf.epw)
+        self._pdf.set_font_size(30)
+        self._pdf.set_text_color(255, 255, 255)
+        self._pdf.text()
 
     def save(self , name):
         self._pdf.output(name)
