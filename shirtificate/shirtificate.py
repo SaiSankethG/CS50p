26d045a9234b1d6a01a6aa55f664ea9bf1c54845
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-13 17:32:01 +0000

    Fri, Jan 13, 2023, 5:32 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -11,7 +11,7 @@ class PDF():
         self._pdf.image("shirtificate.png" ,w=self._pdf.epw)
         self._pdf.set_font_size(30)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text(x=50 , y=100 , txt=f"{name} took CS50")
+        self._pdf.text(x=50 , y=140 , txt=f"{name} took CS50")
 
     def save(self , name):
         self._pdf.output(name)
