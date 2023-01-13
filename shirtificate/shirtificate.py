bb298c4cf451150123715621688df1127d847d80
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-13 17:11:40 +0000

    Fri, Jan 13, 2023, 5:11 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,6 +4,9 @@ from fpdf import FPDF
 # pdf=FPDF(orientation="P" , unit="mm" , format="A4")
 class PDF():
     def __init__(self, name):
+        self._pdf=FPDF()
+        self._pdf.add_page()
+        self._pdf.set_font("helvetica", size=20)
         
 
     def save(self , name):
