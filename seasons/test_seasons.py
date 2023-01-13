4f6a344987f5f47a89302ef75b2745968e5fff81
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-13 18:04:43 +0000

    Fri, Jan 13, 2023, 6:04 PM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,13 +1,13 @@
-from seasons import convert
+from seasons.seasons import convert
 
 def main():
     test_check_minutes()
 
 def test_check_minutes():
-    assert convert("2003-09-01")=="ten million, one hundred forty thousand, four hundred eighty minutes"
+    assert convert("2003-09-01")=="Ten million, one hundred eighty-six thousand, five hundred sixty minutes"
     # with pytest.raise(ValueError):
     #     convert()
-    assert convert("2021-12-12")=="five hundred twenty-five thousand, six hundred minutes"
+    assert convert("2021-12-12")=="Five hundred seventy-one thousand, six hundred eighty minutes"
     # assert convert(")
 
 if __name__=="__main__":
