7a753841d04aed1195fafdfb6b757d598cd4f8de
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-13 15:21:58 +0000

    Fri, Jan 13, 2023, 3:21 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,5 +1,7 @@
 from fpdf import FPDF
 
-name=input("Name: ")
-pdf=FPDF(orientation="P" , unit="mm" , format="A4")
-
+# name=input("Name: ")
+# pdf=FPDF(orientation="P" , unit="mm" , format="A4")
+class PDF(FPDF):
+    def header(self):
+        self.image("../shirtificate.png" , )
