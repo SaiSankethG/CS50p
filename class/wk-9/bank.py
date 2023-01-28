e0847fae5bddefcd2a5bf4bfd333c0ea748bd85d
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-28 19:13:17 +0000

    Sat, Jan 28, 2023, 7:13 PM +00:00

--- class/wk-9/bank.py
+++ class/wk-9/bank.py
@@ -1,18 +1,35 @@
-global bal=0
+# bal=0
 
-def main():
-    print("Balance:", bal)
-    deposit(100)
-    withdraw(50)
-    print("balance:", bal)
+# def main():
+#     print("Balance:", bal)
+#     deposit(100)
+#     withdraw(50)
+#     print("balance:", bal)
 
-def deposit(n):
-    global bal
-    bal+=n
+# def deposit(n):
+#     global bal
+#     bal+=n
 
-def withdraw(n):
-    global bal
-    bal-=n
+# def withdraw(n):
+#     global bal
+#     bal-=n
+
+class account:
+    def __init__(self):
+        self._bal=0
+
+    @property
+    def balance(self):
+        return self._bal
+
+    def deposit(self, n):
+        self._bal+=n
+
+    def withdraw(self, n):
+        self._bal-=n
+
+a=account()
+print("Balance:" a.balance)
 
 if __name__=="__main__":
     main()
