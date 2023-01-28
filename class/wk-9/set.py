c63be10416fbf695b5c3dd75a29a1e64a6b629e8
sai sanketh g <92433198+SaiSankethG@users.noreply.github.com>
2023-01-28 18:21:20 +0000

    Sat, Jan 28, 2023, 6:21 PM +00:00

---
+++ class/wk-9/set.py
@@ -0,0 +1,11 @@
+students=[
+    {name="Harry", house="Gryffindor"},
+    {name="Heromine", house="Gryffindor"},
+    {name="Ron", house="Gryffindor"},
+    {name="Draco", house="Slytherin"},
+    {name="Padma" ,house="Ravenclaw"}
+]
+
+houses=[]
+for student in students:
+    if 
